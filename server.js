9b02548fcb5bd0e1cbb678845442bfa52c4b7d63
DESKTOP-EGDNETJ\chris <chris.simmons854@gmail.com>
2018-04-28 04:58:20 -0500

    no message

--- server.js
+++ server.js
@@ -8,6 +8,19 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
+
+const forceSSL = function() {
+    return function (req, res, next) {
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+            return res.redirect(
+                ['https://', req.get('Host'), req.url].join('')
+            );
+        }
+        next();
+    }
+}
+
+app.use(forceSSL());
 app.listen(process.env.PORT || 8080);
 
 //changed

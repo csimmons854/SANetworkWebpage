9f4404a69c8d93039acb5421c559949b9753ddd5
DESKTOP-EGDNETJ\chris <chris.simmons854@gmail.com>
2018-04-29 11:55:28 -0500

    no message

--- db.js
+++ db.js
@@ -8,8 +8,8 @@ exports.connect = function (done) {
     state.pool = mysql.createPool({
         host: 'sanetwork.ck2egjql3cxl.us-east-2.rds.amazonaws.com',
         user: 'simmonsc',
-        password: 'e330f6ef1a',
-        database: 'group6-4431'
+        password: 'group6-4431',
+        database: 'SADatabase'
     });
     done();
 };

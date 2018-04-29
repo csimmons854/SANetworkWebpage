87913367c5630565c9a00d9ac6782c18eef6703b
DESKTOP-EGDNETJ\chris <chris.simmons854@gmail.com>
2018-04-29 11:49:05 -0500

    no message

--- db.js
+++ db.js
@@ -9,7 +9,7 @@ exports.connect = function (done) {
         host: 'sanetwork.ck2egjql3cxl.us-east-2.rds.amazonaws.com',
         user: 'simmonsc',
         password: 'e330f6ef1a',
-        database: 'SADatabase'
+        database: 'group6-4431'
     });
     done();
 };
